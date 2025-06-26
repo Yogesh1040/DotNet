46bfbf5fa7534acb03cf2a4cb164280ea68514c3
Yogesh Bhapkar <yogeshbhapkar1040@gmail.com>
2025-06-26 13:31:40 +0530

    Add files via upload

---
+++ Day-5/POOrmExample/POOrmExample/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POOrmExample.Models;
+
+public class Customer
+{
+    [Key]
+    public int CustomerId { get; set; }
+    [Required(ErrorMessage ="Contact Name is a required field!")]
+    [MaxLength(100)]
+    public string ContactName { get; set; } = string.Empty;
+    [Required(ErrorMessage = "City is a required field!")]
+    [MaxLength(80)]
+    public string City { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Email Id is a required field!")]
+    [MaxLength(100)]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    public ICollection<Order> Orders { get; set; }
+}
---
+++ Day-5/POOrmExample/POOrmExample/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POOrmExample.Models;
+
+public class Order
+{
+    [Key]
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public DateTime RequiredDate { get; set; }
+    public int CustomerId { get; set; }
+
+    public Customer Customer { get; set; }
+
+}
